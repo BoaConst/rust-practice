9c855abac65461544d62e2cef673e38755ccd330
BoaConst <suumilroy120@gmail.com>
2023-02-27 18:04:07 -0500

    changes

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read input");
+
+    println!("You guessed: {guess}");
+
+    match guess.as_str().cmp(&secret_number.as_str()) {
+        Ordering::Less => println!("Small"),
+        Ordering::Greater => println!("Great"),
+        Ordering::Equal => println!("Equal")
+    }
 }
